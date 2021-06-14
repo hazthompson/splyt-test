abb8204d469362246fda9ac6d78758bde70fa6de
Harriet Thompson <46738131+hazthompson@users.noreply.github.com>
2021-06-14 15:36:04 +0100

    Update <p> tag render and display new title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <p>Splyt Test</p>
         </p>
         <a
           className='App-link'
